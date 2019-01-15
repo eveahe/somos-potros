9177c26e46ccec1cf8452b18d54f6ae1e5108ebe
Glitch (somos-potros) <none>
2019-01-15 07:45:34 +0000

    👦📺 Updated with Glitch

---
+++ public/client.js
@@ -0,0 +1,14 @@
+$("#contact-link").click(function(){
+  $(".text-box").hide();
+  $("#contact-text").show();
+});
+
+$("#about-link").click(function(){
+  $(".text-box").hide();
+  $("#about-text").show();
+});
+
+$("#project-link").click(function(){
+  $(".text-box").hide();
+  $("#project-text").show();
+});
